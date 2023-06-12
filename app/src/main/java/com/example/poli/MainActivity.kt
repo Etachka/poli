7e020f21a6fff9bc574d440102ab06817357db2d
Паша Филькенков <gprogg5566@gmail.com>
2023-06-12 22:42:28 +0400

    wjgiorehjgoirehgju943ty43h78eruigj5erthbu98rehgbiuerhgbieurghreiughreiughiure

--- app/src/main/java/com/example/poli/MainActivity.kt
+++ app/src/main/java/com/example/poli/MainActivity.kt
@@ -5,20 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.button3)
-        button.setOnClickListener {
-            val intent = Intent(this, List::class.java)
-            startActivity(intent)
-        }
+
+        // Переход на окно регистрации
         val buttonReg = findViewById<Button>(R.id.button4)
         buttonReg.setOnClickListener {
             val intent = Intent(this, Registration::class.java)
             startActivity(intent)
+        }
+    }
+
+    // Метод проверки данных для входа
+    private fun validateAndProcessData() {
+        val loginEditText = findViewById<EditText>(R.id.editTextTextPassword)
+        val PasswordEditText = findViewById<EditText>(R.id.editTextTextPassword2)
+
+        val login = loginEditText.text.toString()
+        val confirmPassword = PasswordEditText.text.toString()
+
+        if (login != "UsEr" && confirmPassword != "1212") {
+            val message = "Некорректный ввод"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        } else {
+            val button = findViewById<Button>(R.id.button3)
+            button.setOnClickListener {
+                val intent = Intent(this, kotlin.collections.List::class.java)
+                startActivity(intent)
+            }
         }
     }
 }
